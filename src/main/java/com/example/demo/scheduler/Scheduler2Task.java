fa462ff2ca4e85632a31338efc6a1a0151dc0eff
chenxing <chenxing@ecquaria.com>
2018-07-30 16:41:38 +0800

    add scheduler config定时服务(lession 9)

---
+++ src/main/java/com/example/demo/scheduler/Scheduler2Task.java
@@ -0,0 +1,19 @@
+package com.example.demo.scheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class Scheduler2Task {
+
+    private static final SimpleDateFormat df=new SimpleDateFormat("HH:mm:ss");
+
+    //设置系统每隔六秒启动该任务
+    @Scheduled(fixedRate = 6000)//同@Scheduled(cron = "*/6****?")
+    public void reportCurrentTime(){
+        System.out.println("当前时间："+df.format(new Date()));
+    }
+}
---
+++ src/main/java/com/example/demo/scheduler/SchedulerTask.java
@@ -0,0 +1,17 @@
+package com.example.demo.scheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedulerTask {
+
+    private int count=0;
+
+    //设置系统每隔六秒启动该任务
+    @Scheduled(cron = "*/6 * * * * ?") //或@Scheduled(fixedRate=6000)
+    private void process(){
+        System.out.println("this is scheduler task running:"+(count++));
+    }
+
+}
